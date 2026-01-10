1452e6146e6f3e1a38a10e5d76065d1b3a1d90d0
andersonrocha <ironmanrocha81@gmail.com>
2026-01-10 18:19:15 -0300

    Inicialização das variáveis e criação da lógica para iniciar o app

--- app/src/main/java/com/andersonrocha/appquiz/PerguntasActivity.kt
+++ app/src/main/java/com/andersonrocha/appquiz/PerguntasActivity.kt
@@ -1,12 +1,28 @@
 package com.andersonrocha.appquiz
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class PerguntasActivity : AppCompatActivity() {
+
+    private lateinit var textNome: TextView
+    private lateinit var textContadorPerguntas: TextView
+    private lateinit var textPergunta: TextView
+
+    private lateinit var radioGroupPerguntas: RadioGroup
+    private lateinit var radioPergunta01: RadioButton
+    private lateinit var radioPergunta02: RadioButton
+    private lateinit var radioPergunta03: RadioButton
+
+    private lateinit var btnConfirmar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,24 @@ class PerguntasActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        inicializar()
+
+        val bundle = intent.extras
+        val nome = bundle?.getString("nome_usuario") ?: "Nome não identificado"
+        textNome.text = "Olá, $nome"
+    }
+
+    private fun inicializar() {
+        textNome = findViewById(R.id.text_nome)
+        textContadorPerguntas = findViewById(R.id.text_contador_perguntas)
+        textPergunta = findViewById(R.id.text_pergunta)
+
+        radioGroupPerguntas = findViewById(R.id.radio_group_perguntas)
+        radioPergunta01 = findViewById(R.id.radio_pergunta_01)
+        radioPergunta02 = findViewById(R.id.radio_pergunta_02)
+        radioPergunta03 = findViewById(R.id.radio_pergunta_03)
+
+        btnConfirmar = findViewById(R.id.btn_confirmar)
     }
 }
